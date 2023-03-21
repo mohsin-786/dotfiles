8b58f1f899fbffa19414f0bcbe64c8ca54806b05
mohsin-786 <mohsinabedeen78@gmail.com>
2023-03-21 21:35:33 +0530

    Deleted mess

--- rename.sh
+++
@@ -1,8 +0,0 @@
-for images in $(ls images)
-do
-        if [[ $images = *.jpeg ]]
-        then
-                new=$(echo $images | sed 's/jpeg/jpg/g')
-                mv images/$i images/$new
-        fi
-done
